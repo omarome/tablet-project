2bb4328b31b80344da76ad2ba84a4fcfa8398b40
omar al-mashhadani <almashhadaniomar9@gmail.com>
2020-03-14 01:19:11 +0200

    first-function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NevBar from "./Views/Home/NevBar"
 
 function App() {
   return (
     <div className="App">
       Tablet Application 
+      <NevBar/>
       team work 
     </div>
   );
---
+++ src/Views/Home/NevBar.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function NevBar(){
+    return(
+        <div>
+            <input className="form-control"/>
+        </div>
+    )
+}
