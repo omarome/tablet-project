1e43336a2725fe5af7501112f455187421dbe37f
ohuji <juhsalomaki@gmail.com>
2020-03-25 16:34:43 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NevBar from "./Views/Home/NevBar"
+import { BrowserRouter, Route } from "react-router-dom"
+import Koneasentaja from "./Views/Home/components/Koneasentaja"
+import Koneistaja from "./Views/Home/components/Koneistaja"
 
 
 function App() {
   return (
-    <div className="App">
-      Tablet Application 
-      <NevBar/>
-      team work 
+    <div className="App">   
+      <BrowserRouter>
+      <Route path="/koneasentaja" component={Koneasentaja}></Route>
+      <Route path="/koneistaja" component={Koneistaja}></Route>
+      </BrowserRouter>  
     </div>
-    
+
   );
 }
 
