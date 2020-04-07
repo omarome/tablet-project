db8c8d6d6bba25f1455aca9703ddaee019ff5d01
Tuomas01 <tuomastheikkila@hotmail.fi>
2020-04-07 16:57:39 +0300

    added pictures, added contacts, changed from mediaplayer to reactplayer on metso, updated css and added company information

--- src/Views/components/Metso.js
+++ src/Views/components/Metso.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Mediaplayer from './Mediaplayer';
 import './../../App.css';
 import { Link } from "react-router-dom";
 import ReactPlayer from "react-player";
@@ -19,47 +18,67 @@ class Metso extends Component {
   getCompany = () => {
     fetch("/metso/")
       .then(response => response.json())
-      .then(response => this.setState({ company: response.data }))
+      .then(response => this.setState({ company: response.data[0] }))
       .catch(error => console.log(error))
 
   }
 
   render() {
+    const { company } = this.state;
     return (
-      <div className="container-fluid">
+      <div className="Main container-fluid">
 
-        <h1 className="HeaderFont display-3 mt-4">Neles</h1>
+        <h1 className="header HeaderFont display-3 mt-4">Neles Finland</h1>
         <div className="row mt-5">
-          <div className="col-md-4 offset-md-1">
+          <div className="Lista col-md-4 offset-md-1">
             <h2 className="HeaderFont mt-3 ">Infoa yrityksestä</h2>
-            <div>
-              <p>Metso Oyj on suomalainen prosessiteollisuusalan pörssiyritys. Sen asiakkaisiin kuuluvat kaivos-, kivenmurskaus-,</p>
-              <p>öljy- ja kaasualoilla ja kierrätyksessä sekä paperi-, sellu- ja prosessiteollisuudessa toimivat yritykset.</p>
-           </div>
+            <div className="Infoay">
+              <ul>
+                <li>Metso Flow Control OY -> Neles Finland OY</li>
+                <li>Itsenäinen pörssiyhtiö 1.7.2020 alkaen</li>
+                <li>Koneistamo jaettu kolmeen osioon, tiiviste-, suljin- ja
+                raskaskoneistus </li>
+                <li>Yhteensä n. 60 työstökonetta. Sorveja, jyrsimiä, hiomakoneita ja
+                apulaitteistoa (läppäyskoneita jne.)</li>
+                <li>Työllistää n. 100 henkilöä</li>
+                <li>Tukitoiminnot: ohjelmointi, kehitysorganisaatio (menetelmä,
+                laitteistot, prosessit)</li>
+                <li>Erikoisuutena kappaleisiin tehtävät pinnoitteet ja niiden koneistus</li>
+                <li>Koneistamo palvelee neljää kokoonpanolinjaa (arvovirtaa)</li>
+                <ul>
+                  <li>Ato, Stock, Special & heavy</li>
+                  <li>Lisäksi myös varaosa ja tehtaiden välisen toimitukset kuuluvat koneistamon vastuualueisiin</li>
+                </ul>
+              </ul>
+            </div>
           </div>
-          
-          
-          <div className="col-md-5 offset-1 mt-4">
-            <h2 className="HeaderFont ">Yrityksen vaatimukset</h2>
-          <div className="eka"><p>"Yritykseen vaaditaan..."</p></div>
-        </div>
-        
+
+
+          <div className="vaatimukset col-md-5 offset-1 mt-4">
+            <h2 className="HeaderFont ">Yrityksen osaamisvaatimukset</h2>
+            <div className="eka"><p>"Yritykseen vaaditaan..."</p></div>
+          </div>
+
         </div>
 
         <div className="Mediaplayer col-md-4 offset-md-1 mt-5">
           <h2 className="HeaderFont">Yrityksen esittelyvideo</h2>
-          <Mediaplayer
-           url="https://www.youtube.com/watch?v=g91cYBnkUJY&t=8s" 
-           width="100%" 
-           height="350%" />
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=g91cYBnkUJY&t=8s"
+            width="100%"
+            controls={true}
+            alt="Esittelyvideo yrityksestä" />
         </div>
-        
+
         <div className="kotisivu">
-           <p>Yrityksen kotisivut: <a href="https://www.metso.com/fi/" target="_blank">Neles</a></p>
-        </div>           
+          <p>Yrityksen kotisivut: <a href="https://www.metso.com/fi/" target="_blank">Neles Finland</a></p>
+        </div>
+        <div className="kontaktit col-md-4">
+          <p>Yrityksen yhteystiedot: {company.address}, {company.contactperson}, {company.number}</p>
+        </div>
       </div>
     );
   }
-} 
+}
 
 export default Metso;
