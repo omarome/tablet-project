57835bd820e4661d30d91eeb372a501e9e361265
EetuPasonen <eetu.pasonen@edu.omnia.fi>
2020-04-17 15:02:20 +0300

    Added pages for the rest of the companies

--- src/Views/components/Abb.js
+++ src/Views/components/Abb.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import './../../App.css';
+import { Link } from "react-router-dom";
+import ReactPlayer from "react-player";
 
 class Abb extends Component {
 
     state = {
-
         company: []
         
      }
@@ -19,10 +21,67 @@ class Abb extends Component {
          .catch(error => console.log(error))
 
      }
+     render() {
+        const { company } = this.state;
+        return (
+          <div className="Main container-fluid">
+    
+            <h1 className="HeaderFont display-3 mt-4">{company.name}</h1>
+            <div className="row mt-5">
+              <div className="Lista col-md-4 offset-md-1">
+                <h2 className="HeaderFont mt-3 ">Infoa yrityksestä</h2>
+                <div className="Infoay">
+                  <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <ul>
+                      <li>Ato, Stock, Special & heavy</li>
+                      <li></li>
+                    </ul>
+                    <li>Lisätietoa antaa {company.contactperson}, {company.number},  {company.address}</li>
+                  </ul>
+                </div>
+              </div>
+    
+    
+              <div className="vaatimukset col-md-5 offset-1 mt-4">
+                <h2 className="HeaderFont ">Yrityksen osaamisvaatimukset</h2>
+                <div className="vaatimukset">
+                  <ul>
+                    <li>Koneistuksen perusosaaminen</li>
+                    <li>Koneasennuksen perusosaaminen</li>
+                  </ul>
+                </div>
+              </div>
+    
+            </div>
+    
+            <div className="Mediaplayer col-md-4 offset-md-1 mt-5">
+              <h2 className="HeaderFont">Yrityksen esittelyvideo</h2>
+              <ReactPlayer
+                url=""
+                width="100%"
+                controls={true}
+                alt="Esittelyvideo yrityksestä" />
+            </div>
+    
+            <div className="kotisivu">
+              <p>Yrityksen kotisivut: <a className="LinkText" href="" target="_blank">Abb</a></p>
+            </div>
+          </div>
+        );
+      }
+    }
 
 
     
         
-   }
+   
 
 export default Abb;
--- src/Views/components/Planmecatwo.js
+++ src/Views/components/Planmecatwo.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import './../../App.css';
+import { Link } from "react-router-dom";
+import ReactPlayer from "react-player";
 
 class Planmecatwo extends Component {
 
@@ -20,9 +23,61 @@ class Planmecatwo extends Component {
 
      }
 
-
+     render() {
+        const { company } = this.state;
+        return (
+          <div className="Main container-fluid">
     
-        
-   }
-
+            <h1 className="HeaderFont display-3 mt-4">{company.name}</h1>
+            <div className="row mt-5">
+              <div className="Lista col-md-4 offset-md-1">
+                <h2 className="HeaderFont mt-3 ">Infoa yrityksestä</h2>
+                <div className="Infoay">
+                  <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <ul>
+                      <li>Ato, Stock, Special & heavy</li>
+                      <li></li>
+                    </ul>
+                    <li>Lisätietoa antaa {company.contactperson}, {company.number},  {company.address}</li>
+                  </ul>
+                </div>
+              </div>
+    
+    
+              <div className="vaatimukset col-md-5 offset-1 mt-4">
+                <h2 className="HeaderFont ">Yrityksen osaamisvaatimukset</h2>
+                <div className="vaatimukset">
+                  <ul>
+                    <li></li>
+                    <li></li>
+                  </ul>
+                </div>
+              </div>
+    
+            </div>
+    
+            <div className="Mediaplayer col-md-4 offset-md-1 mt-5">
+              <h2 className="HeaderFont">Yrityksen esittelyvideo</h2>
+              <ReactPlayer
+                url=""
+                width="100%"
+                controls={true}
+                alt="Esittelyvideo yrityksestä" />
+            </div>
+    
+            <div className="kotisivu">
+              <p>Yrityksen kotisivut: <a className="LinkText" href="" target="_blank">Planmecatwo</a></p>
+            </div>
+          </div>
+        );
+      }
+    }
 export default Planmecatwo;
