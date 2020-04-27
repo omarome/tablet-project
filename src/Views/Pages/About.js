f478826cc34b314ed5765c2b88a3b8838f53e841
Tuomas01 <tuomastheikkila@hotmail.fi>
2020-04-27 17:58:53 +0300

    Updated the Tietoa meistä page

--- src/Views/Pages/About.js
+++ src/Views/Pages/About.js
@@ -1,15 +1,36 @@
 import React from 'react'
 
-export default function About(){
+export default function About() {
 
-    return(
+    return (
         <div className="textParagraph ">
-           <h1 style={{ textAlign: 'center'}}>Kymmenien tuhansien oppijoiden koulutustalo</h1>
-           <br>
-           </br>
-<p style={{fontSize: '25px'}}>Omniassa osallistuu ammatilliseen tutkintokoulutukseen vuosittain noin 7 000 oppijaa, ammatilliseen valmentavaan koulutukseen noin 350 opiskelijaa ja aikuislukion opintietä kulkee noin 1 500 opiskelijaa. Aikuisten perusopetuksessa opiskelee vuosittain yli 200 opiskelijaa. Lisä- ja täydennyskoulutusten suorittajia on noin 1 600. Vapaan sivistystyön kursseilla elämäänsä rikastuttaa vuosittain noin 22 500 kursseilla kävijää: kursseja on tarjonnassa jopa 3 000! Yli 300 nuorta löytää vuosittain suunnan elämälleen nuorten työpajoilla. Vieraskielisten koulutuksessa opiskelee vuosittain noin 300 oppijaa. Oppisopimuksen kautta ammatin hankkii noin 1 000 henkilöä.
+            <h1 style={{ textAlign: 'center' }}>Kotu</h1>
+            <img src={require('../../media/kotu.PNG')} alt="Kotu osaamispisteet" />
+            <div className="lista1">
+                <h1>Työelämätahtoinen</h1>
+                <ul>
+                    <li>Suunnittelemme koulutusten sisällöt yhteistyössä <br></br> työelämänkanssa - oppijoita kuullen.</li>
+                    <li>Tarjoamme ratkaisuja työyhteisön osaamisen <br></br> kehittämiseen.</li>
+                    <li>Tarvitsemme työpaikkoja työssäoppijoille.</li>
+                    <li>Kehitämme yrittäjyyttä uudenlaisissa <br></br> oppimisympäristöissä.</li>
+                    <br></br>
+                    <li>Tekniikan alojen kehittämisen strategiahanke 2019-2020</li>
+                </ul>
+            </div>
+            <div className="lista2">
+                <div className="otsikko">
+                    <h1>Yritysyhteistyö Kone- ja <br></br> tuotantotekniikan alalla</h1>
+                    <ul>
+                        <li>Työvoimatarpeen kartoitus</li>
+                        <li>Koulutuksen työelämävastaavuus, <br></br>painopistealueet opetuksessa</li>
+                        <li>Kone- ja laitekannan uusiminen</li>
+                        <br></br>
+                        <li>Työelämässä oppiminen ja näytöt työpaikalla</li>
+                        <li>Koulutussopimuksen rinnalle oppisopimus</li>
+                    </ul>
+                </div>
+            </div>
 
-Tämän kaiken mahdollistaa noin 900 osaajaa, jotka ovat työkseen omnialaisia.</p>
         </div>
     )
 }
